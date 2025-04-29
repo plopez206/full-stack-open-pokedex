aa8e0059332c0ec28808654400b8452d80d741df
Pablo López Fernández <pablo@MacBook-Air-de-Pablo.local>
2025-04-29 14:57:49 -0500

    Fix ESLint issues: allow Jest globals and remove unused Router

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:jest/recommended'
   ],
   parserOptions: {
     ecmaVersion: 12,
@@ -15,6 +17,11 @@ module.exports = {
   plugins: [
     'react', 'jest'
   ],
+  settings: {
+    react: {
+      version: '18'
+    }
+  },
   rules: {
     indent: [
       'error',
