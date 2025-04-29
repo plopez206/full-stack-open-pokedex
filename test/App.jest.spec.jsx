de6bdfd4cd43ddc88adeb7c4feb987c89e54090e
Pablo López Fernández <pablo@MacBook-Air-de-Pablo.local>
2025-04-29 15:08:26 -0500

    Fix tests: correct expected links and remove duplicate Router

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react'
 import axiosMock from 'axios'
 import { act } from 'react-dom/test-utils'
 import '@testing-library/jest-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from '../src/App'
 
 jest.mock('axios')
@@ -18,7 +17,7 @@ describe('<App />', () => {
       }
     )
     await act(async () => {
-      render(<Router><App/></Router>)
+      render(<App/>)
     })
     expect(axiosMock.get).toHaveBeenCalledTimes(1)
     expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
@@ -27,7 +26,7 @@ describe('<App />', () => {
   it('shows error', async () => {
     axiosMock.get.mockRejectedValueOnce(new Error())
     await act(async () => {
-      render(<Router><App/></Router>)
+      render(<App/>)
     })
     expect(screen.getByTestId('error')).toBeVisible()
   })
