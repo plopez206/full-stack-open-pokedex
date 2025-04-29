945adcdf88c43b232cf31e3f23a20127a0e4bb1b
Pablo López Fernández <pablo@MacBook-Air-de-Pablo.local>
2025-04-29 15:10:52 -0500

    Wrap App tests with MemoryRouter

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import axiosMock from 'axios'
 import { act } from 'react-dom/test-utils'
+import { MemoryRouter } from 'react-router-dom' // <-- Agregado aquí
 import '@testing-library/jest-dom'
 import App from '../src/App'
 
@@ -9,15 +10,17 @@ jest.mock('axios')
 
 describe('<App />', () => {
   it('fetches data', async () => {
-    axiosMock.get.mockResolvedValueOnce(
-      {
-        data: {
-          results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
-        }
+    axiosMock.get.mockResolvedValueOnce({
+      data: {
+        results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
       }
-    )
+    })
     await act(async () => {
-      render(<App/>)
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      )
     })
     expect(axiosMock.get).toHaveBeenCalledTimes(1)
     expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
@@ -26,9 +29,12 @@ describe('<App />', () => {
   it('shows error', async () => {
     axiosMock.get.mockRejectedValueOnce(new Error())
     await act(async () => {
-      render(<App/>)
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      )
     })
     expect(screen.getByTestId('error')).toBeVisible()
   })
-
 })
